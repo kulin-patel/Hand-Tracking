90df9945d2f8db2e8c844d808474076bb7dc0b65
kulinpatel <contact@kulinpatel.com>
2021-06-03 20:55:05 +0530

    Got coordinates of landmark points

--- HandTracking.py
+++ HandTracking.py
@@ -12,21 +12,31 @@ cTime = 0
 
 while True:
     success, img = cap.read()
+    img= cv2.flip(img,1)
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
     #print(results.multi_hand_landmarks)
 
     if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks: #handLMs are points. so we need conection too-->mpHands.HAND_CONNECTIONS
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+        for handLms in results.multi_hand_landmarks: 
+        #handLMs are 21 points. so we need conection too-->mpHands.HAND_CONNECTIONS
+            for id, lm in enumerate(handLms.landmark):
+                #print(id, lm)
+                #lm = x,y cordinate of each landmark in float numbers. lm.x, lm.y methods
+                #So, need to covert in integer
+                h, w, c =img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                #print(id, cx, cy)
+                # if id == 4: #(To draw 4th point)
+                #cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS) #drawing points and lines(=handconections)
 
     #Write frame rate
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, "FPS= " + str(int(fps)), (10, 20), cv2.FONT_HERSHEY_PLAIN, 1,
-                (255, 0, 255), 1)
-
+    cv2.putText(img, "FPS= " + str(int(fps)), (10, 20), cv2.FONT_HERSHEY_PLAIN, 1,(0, 0, 0), 1)
+    
     cv2.imshow('image', img)
     if cv2.waitKey(1)==27:
         break
